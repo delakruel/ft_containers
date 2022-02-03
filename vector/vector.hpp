49b3cc4c82711d55c7ad9266cd8e52461c4aaa1d
rloyce <delakruel@gmail.com>
2022-02-03 23:57:44 +0300

    push_back

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -35,8 +35,8 @@ public:
 
 	explicit vector(size_type count, const T& value = T(), const Allocator& alloc = Allocator()) : sz(count), cap(count), alloc(alloc) {
 		try {
-			arr = this->alloc.allocate(count);
-			for (size_type i = 0; i < count; ++i)
+			arr = this->alloc.allocate(cap);
+			for (size_type i = 0; i < cap; ++i)
 				this->alloc.construct((arr + i), value);
 		} catch (std::bad_alloc &e) {
 			std::cerr << e.what() << std::endl;
@@ -45,7 +45,9 @@ public:
 	// template< class InputIt > vector( InputIt first, InputIt last, const Allocator& alloc = Allocator());//5
 	// vector( const vector& other );//6
 
-	~vector() {};
+	~vector() {
+
+	};
 
 	vector<T,Allocator>& operator=(const vector<T,Allocator>& x);
 
@@ -75,22 +77,22 @@ public:
 	// void reserve(size_type n);
 
 	/*		Element access		*/
-	reference at(size_type n) {
-		if (n >= sz || n < 0)
+	reference at(size_type pos) {
+		if (pos >= sz || pos < 0)
 			throw std::out_of_range("ft::vector");
-		return *(arr + n);
+		return *(arr + pos);
 	};
-	const_reference at(size_type n) const {
-		if (n >= sz || n < 0)
+	const_reference at(size_type pos) const {
+		if (pos >= sz || pos < 0)
 			throw std::out_of_range("ft::vector");
-		return *(arr + n);
+		return *(arr + pos);
 	};
 	//sega - eto norm, like a std::vector
-	reference operator[](size_type n) {
-		return *(arr + n);
+	reference operator[](size_type pos) {
+		return *(arr + pos);
 	};
-	const_reference operator[](size_type n) const {
-		return *(arr + n);
+	const_reference operator[](size_type pos) const {
+		return *(arr + pos);
 	};
 
 	reference front() {
@@ -106,8 +108,30 @@ public:
 		return *(arr + sz - 1);
 	};
 
-	// /*		Modifiers		*/
-	// void push_back(const T& x);
+	/*		Modifiers		*/
+	void push_back(const value_type& val) {
+		if (cap == 0) {
+			arr = alloc.allocate(1);
+			alloc.construct(arr, val);
+			++sz;
+			++cap;
+			return ;
+		}
+		if (sz < cap) {
+			alloc.construct(arr + sz, val);
+			++sz;
+			return ;
+		}
+		value_type* newArr = alloc.allocate(cap * 2);
+		for (size_type i = 0; i < sz; ++i) {
+			alloc.construct(newArr + i, *(arr + i));
+			alloc.destroy(arr + i);
+		}
+		alloc.construct(newArr + sz, val);
+		alloc.deallocate(arr, cap);
+		++sz;
+		cap *= 2;
+	};
 	// void pop_back();
 	// iterator insert(iterator position, const T& x);
 	// void insert(iterator position, size_type n, const T& x);
@@ -122,22 +146,22 @@ public:
 
 		/*		Operators		*/
 template <class T, class Allocator>
-	bool operator==(const vector<T,Allocator>& x, const vector<T,Allocator>& y);
+	bool operator==(const vector<T,Allocator>& lhs, const vector<T,Allocator>& rhs);
 template <class T, class Allocator>
-	bool operator< (const vector<T,Allocator>& x, const vector<T,Allocator>& y);
+	bool operator< (const vector<T,Allocator>& lhs, const vector<T,Allocator>& rhs);
 template <class T, class Allocator>
-	bool operator!=(const vector<T,Allocator>& x, const vector<T,Allocator>& y);
+	bool operator!=(const vector<T,Allocator>& lhs, const vector<T,Allocator>& rhs);
 template <class T, class Allocator>
-	bool operator> (const vector<T,Allocator>& x, const vector<T,Allocator>& y);
+	bool operator> (const vector<T,Allocator>& lhs, const vector<T,Allocator>& rhs);
 template <class T, class Allocator>
-	bool operator>=(const vector<T,Allocator>& x, const vector<T,Allocator>& y);
+	bool operator>=(const vector<T,Allocator>& lhs, const vector<T,Allocator>& rhs);
 template <class T, class Allocator>
-	bool operator<=(const vector<T,Allocator>& x, const vector<T,Allocator>& y);
+	bool operator<=(const vector<T,Allocator>& lhs, const vector<T,Allocator>& rhs);
 
 
 		/*		Specialized algorighms		*/
 template <class T, class Allocator>
-	void swap(vector<T,Allocator>& x, vector<T,Allocator>& y);
+	void swap(vector<T,Allocator>& lhs, vector<T,Allocator>& rhs);
 
 }
 
